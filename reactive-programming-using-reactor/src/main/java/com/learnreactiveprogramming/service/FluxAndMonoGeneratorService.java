c94847150d17280d84ad649b43b9e8ce3c9b2484
rnjstjdgh <gshgsh0831@daum.net>
2022-08-03 23:00:33 +0900

    s

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -1,4 +1,33 @@
 package com.learnreactiveprogramming.service;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.Arrays;
+
+
+@Slf4j
 public class FluxAndMonoGeneratorService {
+
+    public Flux<String> namesFlux(){
+        return Flux.fromIterable(Arrays.asList("alex", "ben", "chloe")).log();
+    }
+
+    public Mono<String> namesMono(){
+        return Mono.just("alex");
+    }
+
+    public static void main(String[] args) {
+        FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
+        fluxAndMonoGeneratorService.namesFlux()
+                .subscribe(name -> {
+                    log.info("Name is : " + name);
+                });
+
+        fluxAndMonoGeneratorService.namesMono()
+                .subscribe(name -> {
+                    log.info("Name is : " + name);
+                });
+    }
 }

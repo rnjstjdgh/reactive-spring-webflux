c83a28eed603fe7098fc6e3540e5050f1a16d73e
rnjstjdgh <gshgsh0831@daum.net>
2022-08-10 23:47:40 +0900

    s

--- reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
+++ reactive-programming-using-reactor/src/main/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorService.java
@@ -5,6 +5,8 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
 
 @Slf4j
@@ -18,6 +20,36 @@ public class FluxAndMonoGeneratorService {
         return Mono.just("alex");
     }
 
+    public Flux<String> namesFluxMap(){
+        return Flux.fromIterable(Arrays.asList("alex", "ben", "chloe"))
+//                .map(ele -> ele.toUpperCase())
+                .map(String::toUpperCase);
+    }
+
+    public Flux<String> namesFluxMapImmutabillity(){
+         Flux<String> namesFluxMap = Flux.fromIterable(Arrays.asList("alex", "ben", "chloe"));
+        /***
+         * > 아래 코드가 실행되는 순간 [Arrays.asList("alex", "ben", "chloe")] 이 리스트의 내용이 바뀔까?
+         * > 정답은 NO!
+         *      > 테스트를 해 보면 namesFluxMap 을 구독 하게 되면 여전히 소문자인 것을 확인할 수 있다.
+         *      > 채이닝을 통해 map 을 적용한 그 Flux 를 리턴해야 map 이 적용된다.
+         *      > 즉 아래 2 case 의 flux 는 서로 다르다(map function 도 새로운 Flux 를 리턴한다.)
+         *          > Flux<String> namesFluxMap = Flux.fromIterable(Arrays.asList("alex", "ben", "chloe"));
+         *          > Flux<String> namesFluxMap = Flux.fromIterable(Arrays.asList("alex", "ben", "chloe")).map(~~);
+         * > Reactive Steams 에서 immutable 은 중요한 특성이다.
+         * > 원천 데이터 자체가 변하는 것이 아니라 구독 시점에 Publisher -> Subscriber 로 전달되는 중간에 map 을 통해 변경되는 것
+         */
+        namesFluxMap.map(String::toUpperCase);
+        return namesFluxMap;
+    }
+
+    public Flux<String> namesFluxFilter(int stringLength){
+        return Flux.fromIterable(Arrays.asList("alex","ben","chloe"))
+                .map(String::toUpperCase)
+                .filter(s->s.length() > stringLength)
+                .map(s -> s.length() + " - " + s);
+    }
+
     public static void main(String[] args) {
         FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
         fluxAndMonoGeneratorService.namesFlux()
@@ -29,5 +61,7 @@ public class FluxAndMonoGeneratorService {
                 .subscribe(name -> {
                     log.info("Name is : " + name);
                 });
+
+
     }
 }
--- reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
+++ reactive-programming-using-reactor/src/test/java/com/learnreactiveprogramming/service/FluxAndMonoGeneratorServiceTest.java
@@ -1,7 +1,72 @@
 package com.learnreactiveprogramming.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoGeneratorServiceTest {
+    FluxAndMonoGeneratorService fluxAndMonoGeneratorService = new FluxAndMonoGeneratorService();
 
+    @Test
+    void namesFlux() {
+        // given
+
+
+        // when
+        Flux namesFlux = fluxAndMonoGeneratorService.namesFlux();
+
+        //then
+        /***
+         * > Mono & Flux test 에는 대부분 StepVerifier 가 사용됨
+         * > 즉 퍼블리셔 테스트에는 StepVerifier 가 사용된다고 보면 됨
+         * > 내부적으로 Subscribe 후 onNext 되어 오는 값들을 검증할 것임
+         */
+        StepVerifier.create(namesFlux)
+                .expectNext("alex", "ben")  // Flux 를 내부적으로 구독해 보면서 예상된 데이터와 같은지 확인하는듯
+                .expectNext("chloe")        // 체이닝도 지원
+                .verifyComplete();
+
+        StepVerifier.create(namesFlux)
+                .expectNextCount(3)         // 이러한 다양한 테스트 메소드를 체이닝 형식으로 제공
+                .verifyComplete();
+
+        StepVerifier.create(namesFlux)
+                .expectNext("alex")
+                .expectNextCount(2)         // 위에서 1개를 소비 했으니 2개가 남은 것이 맞음
+                .verifyComplete();
+
+        /***
+         * > 퍼블리셔의 ele 를 전부 소모 안하고 verifyComplete 호출 하면 테스트 실패한다.
+         */
+        StepVerifier.create(namesFlux)
+                .expectNext("alex")
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFluxMap() {
+        Flux namesFluxMap = fluxAndMonoGeneratorService.namesFluxMap();
+
+        StepVerifier.create(namesFluxMap)
+                .expectNext("ALEX","BEN")
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFluxImmutabillity() {
+        Flux<String> namesFluxMap = fluxAndMonoGeneratorService.namesFluxMapImmutabillity();
+        StepVerifier.create(namesFluxMap)
+                .expectNext("ALEX","BEN")
+                .expectNextCount(1)
+                .verifyComplete();
+    }
+
+    @Test
+    void namesFluxMapFilter() {
+        Flux<String> namesFluxMapFilter = fluxAndMonoGeneratorService.namesFluxFilter(3);
+        StepVerifier.create(namesFluxMapFilter)
+                .expectNext("4 - ALEX","5 - CHLOE")
+                .verifyComplete();
+    }
 }

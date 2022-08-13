8f95ccf0a609698297128d4db3562c9f79239599
rnjstjdgh <gshgsh0831@daum.net>
2022-08-13 14:59:11 +0900

    s

---
+++ movies-info-service/src/main/java/com/reactivespring/controller/MovieInfoController.java
@@ -0,0 +1,23 @@
+package com.reactivespring.controller;
+
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.service.MovieInfoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/v1")
+@RequiredArgsConstructor
+public class MovieInfoController {
+
+    private final MovieInfoService movieInfoService;
+
+    @PostMapping("/movieinfos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo){
+        return movieInfoService.addMovieInfo(movieInfo);
+    }
+}
